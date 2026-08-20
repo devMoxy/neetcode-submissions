794957b7c2289b6d8306264dc3f3e929d2a62d93
devMoxy <adelowoenoch1@gmail.com>
2026-08-20 12:12:23 +0100

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] char_count = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            char_count[s.charAt(i) - 'a']++;
+            char_count[t.charAt(i) - 'a']--;
+        }
+        int sum = 0;
+
+        for(int num: char_count){
+            if(num != 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
